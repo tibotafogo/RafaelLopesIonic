75c19c898ddbe5a8ce2dff5f4d69a2a352ae6840
Técnico em Informática - 2022.1 <47127512022.1@senacrjedu.local>
2023-08-11 11:50:44 -0300

    feat: update files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController, PopoverController, ToastController } from '@ionic/angular';
+import { FilmeService } from '../services/filme.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,125 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  type: string = "pending"
 
-  constructor() {}
+  constructor(public alertController: AlertController, public filmeService: FilmeService, 
+    public toastController: ToastController)
+{ }
+
+async presentAlertPromptAdd(){
+  const alert = await this.alertController.create({
+    header: 'Adicionar tarefa',
+    inputs: [
+      {
+        name: 'tarefa',
+        type: 'text',
+        placeholder: 'Tarefa'
+      
+      },
+      {
+        name : 'date',
+        type: 'text',
+        placeholder: 'Tarefa'
+      },
+      {
+       name: 'date',
+       type: 'date',
+       min: '2023-01-01',
+       max: '2025- 12-31' 
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel'
+      },
+      {
+        text: 'Salvar',
+        handler: (alertData) => {
+          if (alertData.tarefa !="") {
+            this.filmeService.addTarefas(alertData.tarefa, alertData.date);
+          }
+          else{
+            this.presentToast();
+            this.presentAlertPromptAdd();
+          }
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
+async presentAlertPromptDelete(index: number) {
+  const alert = await this.alertController.create({
+    header: 'Excluir tarefa',
+    message: 'Deseja realmente excluir a tarefa ?',
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel'
+      },
+      {
+        text: 'Excluir',
+        role: 'cancel'
+      },
+      {
+        text: 'Excluir',
+        handler: () => {
+          this.filmeService.delFilme(index);
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
+async presentAlertPromptUpdate(index: number, tarefa: any) {
+  const alert = await this.alertController.create({
+    header: 'Atualizar tarefa',
+    inputs: [
+      {
+        name: 'tarefa',
+        type: 'text',
+        placeholder: 'Tarefa',
+        value: tarefa.value
+      },
+      {
+        name: 'date',
+        type: 'date',
+        min: '2023-01-01',
+        max: '2025-12-31',
+        value: tarefa.date.getFullYear() + "-" + ((tarefa.date.getMonth() + 1) < 10 ? "0" + tarefa.date.getMonth() + 1 : tarefa.date.getMonth() + 1) + "-" + ((tarefa.date.getDay() + 1) < 10 ? "0" + tarefa.date.getDay() : tarefa.date.getDay())
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel'
+      },
+      {
+        text: 'Salvar',
+        handler: (alertData) => {
+          if (alertData.tarefa != "") {
+            this.filmeService.updateFilmes(index, alertData.tarefa, alertData.date);
+          }
+          else {
+            this.presentToast();
+            this.presentAlertPromptUpdate(index, tarefa);
+          }
+        }
+      }
+    ]
+      });
+    await alert.present();
+}
+async presentToast() {
+  const toast = await this.toastController.create({
+    message: "Preencha a tarefa !",
+    duration: 2000
+  });
+  await toast.present();
+}
 
 }
+
+
--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
+  updateFilmes(index: number, tarefa: any, date: any) {
+    throw new Error('Method not implemented.');
+  }
+  delFilme(index: number) {
+    throw new Error('Method not implemented.');
+  }
   private tarefas: Tarefa[] =[];
 
   constructor() { }
@@ -11,7 +18,18 @@ export class FilmeService {
     return this.tarefas;
   
 }
+public addTarefas(value: string, date: string){
+  date = date.replace(/-/g,"/")
+  let tarefa: Tarefa = {value: value, date: new Date(date), done: false};
+this.tarefas.push(tarefa);
+
+
 }
+
+
+}
+
+
 interface Tarefa{
   value: string
   date: Date;
